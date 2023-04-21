cc6d3ab5b661406900b989c2c29aea9597c2377b
Miguel Garavito <mgaravitocamargo@gmail.com>
2023-04-20 20:56:24 -0500

    edition

--- main.js
+++ main.js
@@ -2,55 +2,45 @@
 //* URL Base
 const baseUrl = "https://ecommercebackend.fundamentos-29.repl.co/";
 //* Mostrar y ocultar carrito
-const carToggle = document.querySelector('.car__toggle');
-const carBlock = document.querySelector('.car__block');
-//* Dibujar productos en la web
+const cartToggle = document.querySelector('.cart_toggle');
+const cartBlock = document.querySelector('.cart_block');
+//* Dibujar productos
 const productsList = document.querySelector('#products-container');
-//* Carrito de compras
-const car = document.querySelector('#car');
-const carList = document.querySelector('#car__list');
-//* Vaciar el carrito
-emptyCarButton = document.querySelector('#empty__cart')
+//* Carrito
+const cart = document.querySelector('#cart');
+const cartList = document.querySelector('#cart_list');
+//* Vaciar carrito
+emptyCartButton = document.querySelector('#empty_cart')
 //? Necesito tener un array que reciba los elementos que debo introducir en el carrito de compras.
-let carProducts = [];
+let cartProducts = [];
 //* Modal
 const modalContainer = document.querySelector('#modal-container');
 const modalElement = document.querySelector('#modal');
 let modalDetails = [];
-
-//* lógica para mostrar y ocultar el carrito.
-carToggle.addEventListener('click', () => {
-  carBlock.classList.toggle("nav__car__visible")
-  //* Cuando NO tiene la clase nav__car__visible, la agrega. Si se le da click nuevamente y detecta la clase, la retira.
+//* lógica para mostrar y ocultar el carrito
+cartToggle.addEventListener('click', () => {
+  cartBlock.classList.toggle("nav_cart_visible")
+  //* Alterna entre poner y quitar la clase
 })
-
 //! Vamos a crear una función que contenga y que ejecute todos los Listeners al inicio de la carga del código.
 eventListenersLoader()
-
 function eventListenersLoader() {
-  //* Cuando se presione el botón "Add to car"
+  //* Cuando se presione el botón "Add to cart"
   productsList.addEventListener('click', addProduct)
-
   //* Cuando se presione el botón "Delete"
-  car.addEventListener('click', deleteProduct)
-
-  //* Cuando se presione el botón "Empty car"
-  emptyCarButton.addEventListener('click', emptyCar)
-
+  cart.addEventListener('click', deleteProduct)
+  //* Cuando se presione el botón "Empty cart"
+  emptyCartButton.addEventListener('click', emptyCart)
   //* Se ejecuta cuando se carga la página
   document.addEventListener('DOMContentLoaded', () => {
-    carProducts = JSON.parse(localStorage.getItem('cart')) || [];
-    carElementsHTML();
+    cartProducts = JSON.parse(localStorage.getItem('cart')) || [];
+    cartElementsHTML();
   })
-
   //* Cuando se presione el botón "View Details"
   productsList.addEventListener('click', modalProduct);
-
-  //* Cuando se de click al botón para cerrar Modal
+  //* Cuando se presione el botón para cerrar la ventana modal
   modalContainer.addEventListener('click', closeModal);
-
 }
-
 function getProducts() {
   axios.get(baseUrl)
     .then(function (response){
@@ -62,24 +52,23 @@ function getProducts() {
     })
 }
 getProducts()
-
 function printProducts(products){
   let html = '';
   for(let i = 0; i < products.length; i++){
     html += `
-    <div class='product__container'>
-      <div class='product__container__img'>
+    <div class='product_container'>
+      <div class='product_container_img'>
         <img src="${products[i].image}" alt="image">
       </div>
-      <div class="product__container__name">
+      <div class="product_container_name">
         <p>${products[i].name}</p>
       </div>
-      <div class="product__container__price">
+      <div class="product_container_price">
         <p>$ ${products[i].price.toFixed(2)}</p>
       </div>
-      <div class="product__container__button">
-        <button class="car__button add__to__car" id="add__to__car" data-id="${products[i].id}">Add to car</button>
-        <button class="product__details" data-id="${products[i].id}">View Details</button>
+      <div class="product_container_button">
+        <button class="cart_button add_to_cart" id="add_to_cart" data-id="${products[i].id}">Add to cart</button>
+        <button class="product_details" data-id="${products[i].id}">View Details</button>
       </div>
     </div>
     `
@@ -87,36 +76,30 @@ function printProducts(products){
   productsList.innerHTML = html
 }
 //* Agregar productos al carrito
-//* 1. Capturar la información del producto al que se le dé click.
+//* 1. Capturar la información del producto clickeado
 function addProduct(event){
-  if(event.target.classList.contains('add__to__car')){
-    //.contains valida si el elemento existe dentro de la clase
+  if(event.target.classList.contains('add_to_cart')){
     const product = event.target.parentElement.parentElement
     // parentElement nos ayuda a acceder al padre inmediatamente superior del elemento.
-    //console.log(product)
-    carProductsElements(product)
+    cartProductsElements(product)
   }
 }
-//* 2. Debo transformar la información HTML a un array de objetos.
-//* 2.1 Debo validar si el elemento seleccionado ya se encuentra dentro del carrito. Si existe, le debo sumar una unidad para que no se repita. 
-function carProductsElements(product){
-
+//* 2. Transoframar a un array de objetos
+//* 2.1 Validar si el elemento está dentro del carrito para no repetirlo
+function cartProductsElements(product){
   const infoProduct = {
     id: product.querySelector('button').getAttribute('data-id'),
     image: product.querySelector('img').src,
-    name: product.querySelector('.product__container__name p').textContent,
-    price: product.querySelector('.product__container__price p').textContent,
-    // textContent me permite pedir el texto que contiene un elemento.
+    name: product.querySelector('.product_container_name p').textContent,
+    price: product.querySelector('.product_container_price p').textContent,
     quantity: 1
   }
   //* Agregar un contador
-  //* Si dentro de carProducts ya existe un ID igual al que tengo previamente alojado en infoProduct, entonces le sumo 1 a la cantidad.
-
   // some, valida si existe algún elemento dentro del array que cumpla la condición
-  if(carProducts.some(product => product.id === infoProduct.id)){
+  if(cartProducts.some(product => product.id === infoProduct.id)){
     // Si el producto al que le doy click en infoProduct ya existe en carProducts, entonces:
-    const product = carProducts.map(product => {
-      // Como tengo un producto que ya existe dentro de carProducts, entonces debo mapearlo y sumarle una unidad a la cantidad del elemento igual.
+    const product = cartProducts.map(product => {
+      // Actualizar quantity
       if(product.id === infoProduct.id){
         product.quantity ++;
         return product;
@@ -124,34 +107,32 @@ function carProductsElements(product){
         return product;
       }
     })
-    carProducts = [...product]
+    cartProducts = [...product]
   } else {
-    carProducts = [...carProducts, infoProduct]
+    cartProducts = [...cartProducts, infoProduct]
   }
-  console.log(carProducts)
-  carElementsHTML()
+  console.log(cartProducts)
+  cartElementsHTML()
 }
-//* 3. Debo Imprimir, pintar, dibujar o mostrar en pantalla los productos dentro del carrito.
-function carElementsHTML(){
-
-  //! Como cada vez que iteramos con forEach creamos un nuevo div, debemos depurar los duplicados reinicializando el contenedor carList con innerHTML vacío. Esto borra todo lo que pueda estar repetido y vuelve a iterar sobre los elementos actualizados en el array de carProducts.
-  carList.innerHTML = "";
-
-  carProducts.forEach(product => {
+//* 3. Mostrar los productos en el carrito
+function cartElementsHTML(){
+  //! Depuramos los duplicados reinicializando el contenedor cartList con innerHTML vacío
+  cartList.innerHTML = "";
+  cartProducts.forEach(product => {
     const div = document.createElement('div');
     // createElement, permite crear etiquetas desde el DOM.
     div.innerHTML = `
-      <div class="car__product">
-        <div class="car__product__image">
+      <div class="cart_product">
+        <div class="cart_product_image">
           <img src="${product.image}">
         </div>
-        <div class="car__product__description">
+        <div class="cart_product_description">
           <p>${product.name}</p>
           <p>Precio: $ ${(parseFloat(product.price.replace(/[^0-9.-]+/g,""))*product.quantity).toFixed(2)}</p>
           <p>Cantidad: ${product.quantity}</p>
         </div>
-        <div class="car__product__button">
-          <button class="delete__product" data-id="${product.id}">
+        <div class="cart_product_button">
+          <button class="delete_product" data-id="${product.id}">
             Delete
           </button>
         </div>
@@ -159,82 +140,74 @@ function carElementsHTML(){
       <hr>
     `;
     // appendChild permite insertar elementos al DOM, muy similar a innerHTML
-    carList.appendChild(div);
+    cartList.appendChild(div);
   })
   productsStorage()
 }
 //* LocalStorage
 function productsStorage() {
-  localStorage.setItem('cart', JSON.stringify(carProducts))
+  localStorage.setItem('cart', JSON.stringify(cartProducts))
 }
-
 //* Eliminar productos del carrito
 function deleteProduct(event){
-  if(event.target.classList.contains('delete__product')){
+  if(event.target.classList.contains('delete_product')){
     const productId = event.target.getAttribute('data-id')
-    carProducts = carProducts.filter(product => product.id !== productId)
-    carElementsHTML()
+    cartProducts = cartProducts.filter(product => product.id !== productId)
+    cartElementsHTML()
   }
 }
-//* Vaciar el carrito completo
-function emptyCar() {
-  carProducts = [];
-  carElementsHTML();
+//* Vaciar el carrito
+function emptyCart() {
+  cartProducts = [];
+  cartElementsHTML();
 }
-
 //* Ventana Modal
 function modalProduct(event){
-  if(event.target.classList.contains('product__details')){
-    modalContainer.classList.add('show__modal')
+  if(event.target.classList.contains('product_details')){
+    modalContainer.classList.add('show_modal')
     const product = event.target.parentElement.parentElement
     modalDetailsElement(product)
   }
 }
-
 function closeModal(event){
-  if(event.target.classList.contains('icon__modal')){
-    modalContainer.classList.remove('show__modal')
+  if(event.target.classList.contains('icon_modal')){
+    modalContainer.classList.remove('show_modal')
     modalElement.innerHTML = "";
     modalDetails = []
   }
 }
-
 function modalDetailsElement(product){
   const infoDetails = [{
     id: product.querySelector('button').getAttribute('data-id'),
     image: product.querySelector('img').src,
-    name: product.querySelector('.product__container__name p').textContent,
-    price: product.querySelector('.product__container__price p').textContent,
+    name: product.querySelector('.product_container_name p').textContent,
+    price: product.querySelector('.product_container_price p').textContent,
   }]
   modalDetails = [...infoDetails]
   modalHTML();
 }
-
 function modalHTML() {
-
   let modalDetailsHTML = "";
-
   for(let element of modalDetails){
     modalDetailsHTML += `
-      <div class="principal__element">
-        <div class="first__modal__section">
-          <div class="first__modal__text">
+      <div class="principal_element">
+        <div class="first_modal_section">
+          <div class="first_modal_text">
             <p>${element.name}</p>
             <p>${element.price}</p>
           </div>
-          <div class="first__modal__colors">
+          <div class="first_modal_colors">
             <p>Colores</p>
             <div>
               <img src="${element.image}">
             </div>
           </div>
-          <div class="first__modal__sizes__text">
+          <div class="first_modal_sizes_text">
             <div>
               <p>Tallas</p>
-              <p>Guía de tallas</p>
             </div>
           </div>
-          <div class="first__modal__sizes">
+          <div class="first_modal_sizes">
             <div>
               <p>S</p>
             </div>
@@ -255,31 +228,23 @@ function modalHTML() {
             </div>
           </div>
         </div>
-        <div class="second__modal__section">
-          <div class="modal__vector"></div>
+        <div class="second_modal_section">
+          <div class="modal_vector"></div>
           <img src="${element.image}">
           
         </div>
       </div>
     `;
   }
-
   modalElement.innerHTML = modalDetailsHTML;
-
 }
-
 //* Local Storage
-
-// Guardar información en local storage
-// localStorage.setItem("apellido", "Betancur")
-
-// //console.log(localStorage.getItem("apellido"))
-
+// localStorage.setItem("Apellido", "Garavito")
+//console.log(localStorage.getItem("Apellido"))
 // const usuario = {
-//   name: 'Alejandro',
-//   age: 31
+//   name: 'Miguel',
+//   age: 19
 // }
 // localStorage.setItem('usuario', JSON.stringify(usuario))
-
 // const usuarioLocal = localStorage.getItem('usuario')
 // console.log(JSON.parse(usuarioLocal))
